6b493bd23a511172e7345f37b6bf309a50d44989
juprietom <juprietom@users.noreply.github.com>
2022-11-24 01:30:39 -0500

    new changes

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 SERVERS = [
-    # [1, '157.240.21.35', 80],
-    [1, 'localhost', 8080],
+    [1, '54.161.216.34', 8080],
+    [2, '52.90.92.36', 8080],
+    # [1, 'localhost', 8080],
     # [2, 'localhost', 8081],
     # [3, 'localhost', 8082]
 ]
 TTL = 10
 ID = 0
-RPORT = 3637
+RPORT = 8000
 RHOST = 'localhost'
 BUFFER_SIZE = 2048
 BUFFER_SIZE_ACK = 248
